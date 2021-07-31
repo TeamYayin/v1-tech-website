d3a62f7a636d519b9235fde043a0c4f66a85c602
py563 <maredukondaprajwal@gmail.com>
2021-07-31 12:35:27 +0000

    maybe fixing spaces

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -23,15 +23,15 @@ const IndexPage = () => (
             <ul className="feature-icons">
               <li className="icon solid fa-laptop">
                 <a href="https://www.linkedin.com/in/vishwastyagi">Vishwas Tyagi</a>
-                Lead Programming Wizard, He is a veteran software engineer with 8 years of experience.
+                , Lead Programming Wizard, He is a veteran software engineer with 8 years of experience.
               </li>
               <li className="icon solid fa-laptop">
                 <a href="https://www.linkedin.com/in/bharani-racharla-2339051a4/">Bharani Racharla</a>
-                PCB Design Intern, She is currently pursuing a Bachelor&#39;s degree in Electronics and Communications Engineering from Osmania University. She hopes to master the circuit designing process and build a successful career as a design engineer.
+                , PCB Design Intern, She is currently pursuing a Bachelor&#39;s degree in Electronics and Communications Engineering from Osmania University.
               </li>
               <li className="icon solid fa-user-secret">
                 <a href="https://prajwalyashasvi.in/">Prajwal</a>
-                Founder and Full Stack Developer, his experience and interests are in Java, Cybersecurity, Python, AI and ML.
+                , Founder and Full Stack Developer, his experience and interests are in Java, Cybersecurity, Python, AI and ML.
               </li>
               <li className="icon solid fa-mobile">Shylaja, Co-Founder</li>
             </ul>
